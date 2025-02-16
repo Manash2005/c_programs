b031c951b56dca6872e1b71c5380f00db4ee82f0
Manash2005 <swainm099@gmail.com>
2025-02-16 12:16:04 +0530

    solved prog - 26

---
+++ class_work.c/patter_printing/prog-26.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=5;++i)
+    {
+        for(int k=1;k<=5-i+1;k++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;++j)
+        {
+            if(i==1||i==5||j==1||j==2*i-1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
